4d921909de39e9936cb25f36f6faa2039e49deca
Jerry Edwards <jerryedwards@Jerrys-MacBook-Pro.local>
2017-07-13 10:09:57 -0700

    update for design session

--- Sites/Server.py
+++ Sites/Server.py
@@ -1,10 +1,12 @@
 import socketserver
 
+from DataContracts.ProfileInstance import ProfileInstance
 from VerbHandler import VerbHandler
 
 PORT = 8000
 
 Handler = VerbHandler
+profileInstance = ProfileInstance.getInstance()
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
