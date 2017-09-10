2f743374290c3a098879f40b3c1cee60f5fa16dc
Robert Stickney <RobertStickney@users.noreply.github.com>
2017-09-10 16:44:02 -0700

    testing in TS_registersControlStub

--- Sites/ThreadControls/TsRegistersControlStub.py
+++ Sites/ThreadControls/TsRegistersControlStub.py
@@ -75,9 +75,11 @@ class TsRegistersControlStub(Thread):
 
     def read_analog_in(self):
         (first_channel, fifo_depth) = self.ts_reg.adc_fifo_status()
-        debugPrint(4, "FIFO depth: {:d};  First Ch: {:d}".format(fifo_depth, first_channel))
+        debugPrint(4, "FIFO depth: {:d};  First Ch: {:d}; Time: {:0.2f}".format(fifo_depth,
+                                                                                first_channel,
+                                                                                time.time()-self.time_test))
+        self.time_test = time.time()
         while fifo_depth < 16:
-            self.time_test = time.time()
             debugPrint(4,"FIFO depth: {:d}".format(fifo_depth))
             time.sleep(self.adc_period * int(8 - (fifo_depth / 2)))
             (first_channel, fifo_depth) = self.ts_reg.adc_fifo_status()
