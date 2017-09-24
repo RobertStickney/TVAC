2fcaefff0ec87b295dd7c9b3f88246f24e803be2
root <root@ts7250-4d5af2.(none)>
2017-09-24 04:09:21 +0000

    TsRegistersControlStub.py

--- Sites/ThreadControls/TsRegistersControlStub.py
+++ Sites/ThreadControls/TsRegistersControlStub.py
@@ -61,7 +61,7 @@ class TsRegistersControlStub(Thread):
                         
                         Logging.logEvent("Debug","Status Update", 
                            {"message": "Reading and writing with PC 104",
-                             "level":4})
+                             "level":5})
 
                         self.ts_reg.do_write4([self.da_io.digital_out.get_c1_b0(),
                                                self.da_io.digital_out.get_c1_b1(),
