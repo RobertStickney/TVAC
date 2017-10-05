0410eb57285d63922f89064609300d82ee0b1706
Robert Stickney <RobertStickney@users.noreply.github.com>
2017-10-05 14:16:45 -0700

    writing TDK Lambda driver

---
+++ Sites/Tdk_lamda/Tdk_lamda_Genesys.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.5
+
+import io
+import time
+
+
+class Tdk_lambda_Genesys:
+
+    def send_cmd(self, command):
+        tdk = open('/dev/ttyxuart4', 'r+b', buffering=0)
+        for tries in range(1, 3+1):
+            tdk.write(self.append_checksum(command).encode())
+            time.sleep(0.10 * tries)
+            # TODO: Change to error event print("C:--" + self.GenCmd(Command).replace('\r', r'\r') + "---")
+            (resp_good, resp) = self.check_checksum(tdk.read(64).decode())
+            print(resp)
+            if resp_good:
+                break
+            print("TDK LAMBDA cmd try number: {:d}".format(tries))
+        else:
+            print("No more tries! Something is wrong!")
+            resp = ""
+        tdk.close()
+        return resp
+
+    def append_checksum(self, cmd):
+        return '{:s}${:2X}\r'.format(cmd, 0xff & sum(cmd.encode()))
+
+    def check_checksum(self, resp):
+        if resp == self.append_checksum(resp[:-4]):
+            return True, resp[:-4].strip()
+        else:
+            return False, resp.strip()
+
+
+
+if __name__ == '__main__':
+    tdk = Tdk_lambda_Genesys()
+    cmds = ['ADR 1', 'IDN?', 'REV?', 'SN?', 'DATE?', 'OUT?', 'AST?', 'DVC?', 'STT?',
+            'ADR 1', 'IDN?', 'REV?', 'SN?', 'DATE?', 'OUT?', 'AST?', 'DVC?', 'STT?',
+            'ADR 1', 'IDN?', 'REV?', 'SN?', 'DATE?', 'OUT?', 'AST?', 'DVC?', 'STT?']
+    for cmd in cmds:
+        print('cmd: "{:s}" - resp: "{:s}"'.format(cmd, tdk.send_cmd(cmd)))
