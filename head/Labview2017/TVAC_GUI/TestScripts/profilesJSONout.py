a2dbd2d6c76d17204cd21d0dbaa3af3b9e5876a2
stefan27uf <stefan27uf@gmail.com>
2017-10-26 13:13:20 -0700

    Load Database additions

--- head/Labview2017/TVAC_GUI/TestScripts/profilesJSONout.py
+++ head/Labview2017/TVAC_GUI/TestScripts/profilesJSONout.py
@@ -12,7 +12,10 @@ class MySQlConnect:
 
 
 	def __init__(self):
-		userName = os.environ['LOGNAME']
+		if os.name == "posix":
+			userName = os.environ['LOGNAME']
+		else:
+			userName="user"
 		if "root" in userName or (len(sys.argv) > 1 and sys.argv[1] =="--live"):
 			user = "TVAC_Admin"
 			host = "192.168.99.10"
