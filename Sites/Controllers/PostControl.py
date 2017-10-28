aecc72de7b4e8731c6ff1fef53bc09d0565d1241
Robert Stickney <RobertStickney@users.noreply.github.com>
2017-10-28 02:18:13 -0700

    Writing engr interfaces: Shi_Cryopump_interface.vi

--- Sites/Controllers/PostControl.py
+++ Sites/Controllers/PostControl.py
@@ -50,7 +50,22 @@ class PostControl:
         threadInstance.threadCollection.calculateRamp(data)
         return "{'result':'success'}"
 
-    ## Todo: These functions are for engeering tests only Remove from deliverable.
+    def SendHwCmd(self, data):
+        if type(data) is not dict:
+            return '{"result":"Needs a json dictionary of a cmds."}'
+        hw = HardwareStatusInstance.getInstance()
+        Logging.debugPrint(3,"POST: SendHwCmd '%s'" % data)
+        for cmd_target in data.keys():
+            if cmd_target == "Shi_MCC_Cmds":  # ['cmd', arg, arg,... arg]
+                hw.Shi_MCC_Cmds.append(data(cmd_target))
+            elif cmd_target == "Shi_Compressor_Cmds":  # 'cmd'
+                hw.Shi_Compressor_Cmds.append(data(cmd_target))
+            elif cmd_target == "TdkLambda_Cmds":  # ['cmd', arg, arg,... arg]
+                hw.TdkLambda_Cmds.append(data(cmd_target))
+            else:
+                return '{"result":"Unknown Hardware Target."}'
+        return '{"result":"success"}' '{"Shi_MCC_Cmds":["%s","%s",%d]}'
+
     def setPC104_Digital(self, data):
         pins = HardwareStatusInstance.getInstance().PC_104
         Logging.debugPrint(3,"POST: setPC104_Digital '%s'" % data)
